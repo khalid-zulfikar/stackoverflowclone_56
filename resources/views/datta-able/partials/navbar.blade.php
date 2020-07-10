7fdbf48d1d5ff01186d17953bf9bee9695697db1
Muhamad Zulfikar <muhamadzulfikar22@gmail.com>
2020-07-10 08:26:51 +0700

    update navbar

--- resources/views/datta-able/partials/navbar.blade.php
+++ resources/views/datta-able/partials/navbar.blade.php
@@ -38,37 +38,11 @@
                     <li class=""><a href="#" class="">Forum</a></li>
                 </ul>
             </li>
-            <li class="nav-item pcoded-hasmenu">
-                <a href="javascript:" class="nav-link ">
-                    <span class="pcoded-micon"><i class="feather icon-users"></i></span>
-                    <span class="pcoded-mtext">Answer</span>
+            <li class="nav-item">
+                <a href="index.html" class="nav-link ">
+                    <span class="pcoded-micon"><i class="feather icon-user"></i></span>
+                    <span class="pcoded-mtext">My-Answer</span>
                 </a>
-                <ul class="pcoded-submenu">
-                    <li class=""><a href="#" class="">ASN</a></li>
-                    <li class=""><a href="#" class="">Tenaga Ahli</a></li>
-                </ul>
-            </li>
-            <li class="nav-item pcoded-hasmenu">
-                <a href="javascript:" class="nav-link ">
-                    <span class="pcoded-micon"><i class="feather icon-server"></i></span>
-                    <span class="pcoded-mtext">Information</span>
-                </a>
-                <ul class="pcoded-submenu">
-                    <li class=""><a href="#" class="">Struktur Organisasi</a></li>
-                    <li class=""><a href="#" class="">Aplikasi</a></li>
-                    <li class=""><a href="#" class="">Tupoksi</a></li>
-                </ul>
-            </li>
-            <li class="nav-item pcoded-hasmenu">
-                <a href="javascript:" class="nav-link ">
-                    <span class="pcoded-micon"><i class="feather icon-sidebar"></i></span>
-                    <span class="pcoded-mtext">News</span>
-                </a>
-                <ul class="pcoded-submenu">
-                    <li class=""><a href="#" class="">Kategori</a></li>
-                    <li class=""><a href="#" class="">Foto</a></li>
-                    <li class=""><a href="#" class="">Video</a></li>
-                </ul>
             </li>
             <li data-username="Disabled Menu" class="nav-item disabled">
                 <a href="javascript:" class="nav-link"><span class="pcoded-micon">
